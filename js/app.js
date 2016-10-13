7e109bbdd6bc72cf28361c53d5bcbc1941b4d8f5
Russell Sabia <rjsabia@gmail.com>
2016-10-12 19:29:52 -0700

    This push is after hadouken, no sound yet

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+// This function activates ryo when hovered
 $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
@@ -6,6 +8,22 @@ $(document).ready(function() {
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+	})
+	// This function is for ryo when clicked
+	// He gets into throw with hadouken fireball
+	.mousedown(function() {
+		// console.log('mousedown')
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+	})
+	// This returns ryu to ready position
+	.mouseup(function() {
+		// console.log('mouseup')
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
 	});
 });
+// ***************************************
+
 
