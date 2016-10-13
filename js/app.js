2ea2aa76923a947df9d27adc3e14d5bfb81ba03b
Russell Sabia <rjsabia@gmail.com>
2016-10-13 10:18:17 -0700

    Push for mentor

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
 
 // This function activates ryo when hovered
 $(document).ready(function() {
-	$('.ryu').mouseenter(function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	// This function is for ryo when clicked
-	// He gets into throw with hadouken fireball
-	.mousedown(function() {
-		// console.log('mousedown')
-		// play hadouken sound
-		playHadouken();
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').finish().show().animate( 
-			{'left': '1020px'},
-			500,
-			function() {
-				$('.hadouken').hide();
-				$('.hadouken').css('left', '610px');
-			}
-		);
-	})
-	// This returns ryu to ready position
-	.mouseup(function() {
-		// console.log('mouseup')
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-	});
+	// $('.ryu').mouseenter(function() {
+	// 	$('.ryu-still').hide();
+	// 	$('.ryu-ready').show();
+	// })
+	// .mouseleave(function() {
+	// 	$('.ryu-ready').hide();
+	// 	$('.ryu-still').show();
+	// })
+	// // This function is for ryo when clicked
+	// // He gets into throw with hadouken fireball
+	// .mousedown(function() {
+	// 	// console.log('mousedown')
+	// 	// play hadouken sound
+	// 	playHadouken();
+	// 	$('.ryu-ready').hide();
+	// 	$('.ryu-throwing').show();
+	// 	$('.hadouken').finish().show().animate( 
+	// 		{'left': '1020px'},
+	// 		500,
+	// 		function() {
+	// 			$('.hadouken').hide();
+	// 			$('.hadouken').css('left', '610px');
+	// 		}
+	// 	);
+	// })
+	// // This returns ryu to ready position
+	// .mouseup(function() {
+	// 	// console.log('mouseup')
+	// 	$('.ryu-throwing').hide();
+	// 	$('.ryu-ready').show();
+	// });
 	$(document).keydown(function(e) {
 		if (e.keyCode == 88) {
 			// this is where music function go's
@@ -39,10 +39,11 @@ $(document).ready(function() {
 			$('.ryu-cool').show();
 		}
 	}).keyup(function(e) {
-		// if (e.keyCode == 88) {
-			$('ryu-cool').hide();
-			$('ryu-still').show();
-		// }
+		if (e.keyCode == 88) {
+			// $('ryu-still').show();
+			$('.ryu-ready').show();
+			$('.ryu-cool').hide();
+		 }
 	});
 });
 function playHadouken () {
